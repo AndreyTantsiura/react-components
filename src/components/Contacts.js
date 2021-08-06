f7a9a95f696760c9bb5012fc65b02eac0c69a796
Andrew <https://github.com/AndreyTantsiura>
2021-08-06 21:36:54 +0300

    add filtered gender contacts

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -2,48 +2,49 @@ import React, { useState, useEffect } from "react";
 import "./contacts.css";
 import Contact from "./Contact";
 
-function Contacts() {
-  const contacts = [
-    {
-      firstName: "Барней",
-      lastName: "Стинсовський",
-      phone: "+380956319521",
-      gender: "male",
-    },
-    {
-      firstName: "Робін",
-      lastName: "Щербатська",
-      phone: "+380931460123",
-      gender: "female",
-    },
-    {
-      firstName: "Анонімний",
-      lastName: "Анонімус",
-      phone: "+380666666666",
-    },
-    {
-      firstName: "Лілія",
-      lastName: "Олдровна",
-      phone: "+380504691254",
-      gender: "female",
-    },
-    {
-      firstName: "Маршен",
-      lastName: "Еріксонян",
-      phone: "+380739432123",
-      gender: "male",
-    },
-    {
-      firstName: "Теодор",
-      lastName: "Мотсбес",
-      phone: "+380956319521",
-      gender: "male",
-    },
-  ];
+const contacts = [
+  {
+    firstName: "Барней",
+    lastName: "Стинсовський",
+    phone: "+380956319521",
+    gender: "male",
+  },
+  {
+    firstName: "Робін",
+    lastName: "Щербатська",
+    phone: "+380931460123",
+    gender: "female",
+  },
+  {
+    firstName: "Анонімний",
+    lastName: "Анонімус",
+    phone: "+380666666666",
+  },
+  {
+    firstName: "Лілія",
+    lastName: "Олдровна",
+    phone: "+380504691254",
+    gender: "female",
+  },
+  {
+    firstName: "Маршен",
+    lastName: "Еріксонян",
+    phone: "+380739432123",
+    gender: "male",
+  },
+  {
+    firstName: "Теодор",
+    lastName: "Мотсбес",
+    phone: "+380956319521",
+    gender: "male",
+  },
+];
 
+function Contacts() {
   const [contactsArr, setContacts] = useState(contacts);
   const [search, setSearch] = useState("");
   const [filteredContacts, setFilteredContacts] = useState([]);
+  const [checked, setChecked] = useState(["male", "female", "undetermined"]);
 
   const handleSearchChange = (e) => {
     setSearch(e.target.value);
@@ -60,6 +61,31 @@ function Contacts() {
     setFilteredContacts(filteredContacts);
   }, [search, contactsArr]);
 
+  const handleCheckBoxes = (e) => {
+    setChecked(e.target.id);
+  };
+
+  useEffect(() => {
+    if (checked === "male") {
+      const filteredGenderContacts = Object.values(contacts).filter((contact) => {
+        return contact.gender !== "male";
+      });
+      setFilteredContacts(filteredGenderContacts);
+    }
+    if (checked === "female") {
+      const filteredGenderContacts = Object.values(contacts).filter((contact) => {
+        return contact.gender !== "female";
+      });
+      setFilteredContacts(filteredGenderContacts);
+    }
+    if (checked === "undetermined") {
+      const filteredGenderContacts = Object.values(contacts).filter((contact) => {
+        return contact.gender !== undefined;
+      });
+      setFilteredContacts(filteredGenderContacts);
+    }
+  }, [checked]);
+
   return (
     <div className="wrapper">
       <div className="iphone_menu">
@@ -70,29 +96,34 @@ function Contacts() {
           placeholder="&#x2315; search.."
           onChange={handleSearchChange}
         />
+
         <div className="checkboxes">
           <input
             type="checkbox"
             id="male"
             name="male"
-            defaultChecked="checked"
+            defaultChecked={checked}
+            onChange={handleCheckBoxes}
           />
           <label htmlFor="male">Male</label>
           <input
             type="checkbox"
             id="female"
             name="female"
-            defaultChecked="checked"
+            defaultChecked={checked}
+            onChange={handleCheckBoxes}
           />
           <label htmlFor="female">Female</label>
           <input
             type="checkbox"
             id="undetermined"
             name="undetermined"
-            defaultChecked="checked"
+            defaultChecked={checked}
+            onChange={handleCheckBoxes}
           />
           <label htmlFor="undetermined">Undetermined</label>
         </div>
+
         <div className="inner">
           {filteredContacts.map((contact) => {
             return (
